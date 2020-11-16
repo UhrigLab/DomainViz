548747c9ca1a6a9dec0c8771b89d2f2c7ec3adbe
Cameron Ridderikhoff <cameronridderikhoff@gmail.com>
2020-11-15 20:09:28 -0700

    Deployed site to server, altered views.py mainly

---
+++ website-glen/api/api/models.py
@@ -0,0 +1,11 @@
+from api.__init__ import db
+
+# class ImageFile(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     userID = db.Column(db.Integer)
+#     filepath = db.Column(db.String(500))
+
+class PDFModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    resultID = db.Column(db.String(36)) #the unique id i generate in React is 36 characters long
+    file = db.Column(db.LargeBinary)
