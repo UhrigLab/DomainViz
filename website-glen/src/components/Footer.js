8f228db900611515d378caa257a84a257a9267ae
cameronridderikhoff <cameronridderikhoff@gmail.com>
2020-12-08 22:34:37 -0700

    Removed broken import

--- website-glen/src/components/Footer.js
+++ website-glen/src/components/Footer.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Typography, Grid, Button, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { ReactComponent as UalbertaLogo } from './svg/UalbertaLogo.svg';
-import test from './svg/UA-SCI-1C-SOLID-REVERSE.png'
+import UalbertaLogo from './svg/UA-SCI-1C-SOLID-REVERSE.png'
 import CopyrightIcon from '@material-ui/icons/Copyright';
 
 const useStyles = makeStyles((theme) => ({
@@ -29,7 +28,7 @@ export const Footer = () => {
         <Container className={classes.container} maxWidth="xl" disableGutters style={{ marginTop: "20px" }}>
             <Grid container spacing={3} alignItems='center' justify='center'>
                 <Grid item xs={6}>
-                    <img src={test} className={classes.img}></img>
+                    <img src={UalbertaLogo} className={classes.img}></img>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography className={classes.typography} variant='body1'>Contact:</Typography>
