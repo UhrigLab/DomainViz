82603da314191e00baca6b8b96f33e68a6bb3293
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-01-25 22:40:25 -0700

    Adding check to redeploy to find the number running instances of gunicorn (what prodoplot creates

--- website-glen/redeploy.sh
+++ website-glen/redeploy.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-npm run build
-systemctl reload nginx
-systemctl daemon-reload
-systemctl restart prodoplot
+NUM_INSTANCES = (ps -ef | grep "gunicorn" | egrep -v "grep|vi|more|pg" | wc -l)
+ if [ $NUM_INSTANCES -gt 2 ]; 
+ then 
+ echo "running" 
+ echo $NUM_INSTANCES
+ ; 
+ else echo "stopped"; 
+ fi
+
+# cd ..
+# git pull
+# cd website-glen/
+# npm run build
+# systemctl reload nginx
+# systemctl daemon-reload
+# systemctl restart prodoplot
+# echo "Redeployment finished."
