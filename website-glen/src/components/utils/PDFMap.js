acaad4ebe2144145937e79a9c1c523684f76c887
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-03-07 09:48:34 -0700

    Single group will start open on results page, multiple groups will all start closed

--- website-glen/src/components/utils/PDFMap.js
+++ website-glen/src/components/utils/PDFMap.js
@@ -42,44 +42,27 @@ export const PDFMap = ({ images, uid, groupNames }) => {
                     <>
                         {(index % 6 === 0) && //For 2 pdfs in a single row, 6 pdfs per group
                         <Grid item xs={12}>
-                        <Accordion defaultExpanded={index==0} className={classes.accordion}>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
-                                <Typography variant="h5">Group: {groupNames[index/6]}</Typography>
-                                
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <div className={classes.column}>
-                                    <PDF pdf={images[index].file}/>
-                                    <PDF pdf={images[index+2].file}/>
-                                    <PDF pdf={images[index+4].file}/>
-                                </div>
-                                <div className={classes.column}>
-                                    <PDF pdf={images[index+1].file}/>
-                                    <PDF pdf={images[index+3].file}/>
-                                    <PDF pdf={images[index+5].file}/>
-                                </div>
-                            </AccordionDetails>
-                            {/* {(index % 3 === 0) && // For 3 pdfs in a row, 3 pdfs per group
-                                <Grid item xs={12} alignItems='center'>
-                                        <Paper className={classes.paper} variant='outlined'>
-                                            <Typography variant="h5">Group: {groupNames[index/3]}</Typography>
-                                        </Paper>
-                                </Grid>
-                            }
-                            <Grid item xs={4}>
-                                <PDF pdf={image.file}></PDF>
-                            </Grid> */}
-                            {/* {(index % 6 === 0) && //For 2 pdfs in a single row, 6 pdfs per group
-                                <Grid item xs={12} alignItems='center'>
-                                        <Paper className={classes.paper} variant='outlined'>
-                                            <Typography variant="h5">Group: {groupNames[index/6]}</Typography>
-                                        </Paper>
-                                </Grid>
-                            } */}
-                            {/* <Grid item xs={6}>
-                                <PDF pdf={image.file}></PDF>
-                            </Grid> */}
-                        </Accordion>
+                             {/* If there is only a single group, there will only be 6 pdfs in total, 
+                                and if this is the case, start with the accordion expanded, but if there are more than one
+                                groups, then all of the accordions should be closed. */}
+                            <Accordion defaultExpanded={images.length===6} className={classes.accordion}>
+                                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                                    <Typography variant="h5">Group: {groupNames[index/6]}</Typography>
+                                    
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <div className={classes.column}>
+                                        <PDF pdf={images[index].file}/>
+                                        <PDF pdf={images[index+2].file}/>
+                                        <PDF pdf={images[index+4].file}/>
+                                    </div>
+                                    <div className={classes.column}>
+                                        <PDF pdf={images[index+1].file}/>
+                                        <PDF pdf={images[index+3].file}/>
+                                        <PDF pdf={images[index+5].file}/>
+                                    </div>
+                                </AccordionDetails>
+                            </Accordion>
                         </Grid>
                         }
                         </>
